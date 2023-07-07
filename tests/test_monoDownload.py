f8364e1ca2bbbd462e7596988946a93d2003bfe0
daliondev <d4lion.dev@gmail.com>
2023-07-06 21:02:09 -0500

    Feat: test quality doesnt exist

--- tests/test_monoDownload.py
+++ tests/test_monoDownload.py
@@ -1,13 +1,27 @@
 import pytest
+import unittest
 
+from pytest import MonkeyPatch
+from unittest.mock import patch
+from os import getcwd, listdir, path
 from MonoLoad.src.MonoDownloads import audio_download as audio
 
 @pytest.mark.parametrize(
-        ("x", "y", "z"),
+        ("url", "song_name"),
     [
-        ("q","w","e")
+        ("https://www.youtube.com/watch?v=ARWg160eaX4&pp=ygUJYmFkIGJ1bm51","Neverita"),
+        ("https://www.youtube.com/watch?v=ws00k_lIQ9U&pp=ygUJYmFkIGJ1bm51", "Soy Peor"),
+        ("https://www.youtube.com/watch?v=CPK_IdHe1Yg&pp=ygUJYmFkIGJ1bm51", "Si Veo a Tu MaMá"),
+        ("https://www.youtube.com/watch?v=kLpH1nSLJSs&pp=ygUJYmFkIGJ1bm51", "Amorfoda"),
+        ("https://www.youtube.com/watch?v=XA9XGAG6CXM&pp=ygUGbWFsb3J5", "Malory"),
+        ("https://www.youtube.com/watch?v=SAvVnVNSvDk&pp=ygUGbWFsb3J5", "Mujeriego")
     ]
 )
 
-def test_if_quality_typed_doesnt_exist(x,y,z):
-    assert x == x
+def test_if_quality_typed_doesnt_exist(url, song_name):
+    with patch('builtins.input', return_value='1kbps'):
+        audio.download(url=url)
+    assert path.exists(f"{getcwd()}/downloads/audios/{song_name}.mp3")
+
+def test_download_and_save(url, song_name):
+    pass
