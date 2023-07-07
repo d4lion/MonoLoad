a9d10a97a0c007b6ed49f8e61211a4edf8ef3460
daliondev <d4lion.dev@gmail.com>
2023-07-06 21:25:42 -0500

    Fix: Unused function

--- tests/test_monoDownload.py
+++ tests/test_monoDownload.py
@@ -7,21 +7,19 @@ from os import getcwd, listdir, path
 from MonoLoad.src.MonoDownloads import audio_download as audio
 
 @pytest.mark.parametrize(
-        ("url", "song_name"),
+        ("url", "song_name", "quality"),
     [
-        ("https://www.youtube.com/watch?v=ARWg160eaX4&pp=ygUJYmFkIGJ1bm51","Neverita"),
-        ("https://www.youtube.com/watch?v=ws00k_lIQ9U&pp=ygUJYmFkIGJ1bm51", "Soy Peor"),
-        ("https://www.youtube.com/watch?v=CPK_IdHe1Yg&pp=ygUJYmFkIGJ1bm51", "Si Veo a Tu MaMá"),
-        ("https://www.youtube.com/watch?v=kLpH1nSLJSs&pp=ygUJYmFkIGJ1bm51", "Amorfoda"),
-        ("https://www.youtube.com/watch?v=XA9XGAG6CXM&pp=ygUGbWFsb3J5", "Malory"),
-        ("https://www.youtube.com/watch?v=SAvVnVNSvDk&pp=ygUGbWFsb3J5", "Mujeriego")
+        ("https://www.youtube.com/watch?v=ARWg160eaX4&pp=ygUJYmFkIGJ1bm51","Neverita", "48kbps"),
+        ("https://www.youtube.com/watch?v=ws00k_lIQ9U&pp=ygUJYmFkIGJ1bm51", "Soy Peor", "128kbps"),
+        ("https://www.youtube.com/watch?v=CPK_IdHe1Yg&pp=ygUJYmFkIGJ1bm51", "Si Veo a Tu MaMá", "50kbps"),
+        ("https://www.youtube.com/watch?v=kLpH1nSLJSs&pp=ygUJYmFkIGJ1bm51", "Amorfoda", "70kbps"),
+        ("https://www.youtube.com/watch?v=XA9XGAG6CXM&pp=ygUGbWFsb3J5", "Malory", "160kbps"),
+        ("https://www.youtube.com/watch?v=SAvVnVNSvDk&pp=ygUGbWFsb3J5", "Mujeriego", "48kbps")
     ]
 )
 
-def test_if_quality_typed_doesnt_exist(url, song_name):
-    with patch('builtins.input', return_value='1kbps'):
+def test_all_qualities(url, song_name, quality):
+    with patch('builtins.input', return_value=quality):
         audio.download(url=url)
     assert path.exists(f"{getcwd()}/downloads/audios/{song_name}.mp3")
 
-def test_download_and_save(url, song_name):
-    pass
