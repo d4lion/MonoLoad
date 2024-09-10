f6461e4d892ec063c7d427d387ab07a2b4d7fe77
d4lion <d4lion.dev@gmail.com>
2024-09-10 11:53:07 -0500

    :recycle: feat: router map and Routing functions

--- MonoLoad/main.py
+++ MonoLoad/main.py
@@ -1,7 +1,6 @@
 import flet as ft
-from src.MonoDownloads.audio_dowload_gui import get_audio_resolution, download_audio
 from src.transformations import transform_config_to_dict
-from time import sleep
+from src.Routes import Router
 
 # Config
 CONFIG: dict = transform_config_to_dict()
@@ -12,6 +11,7 @@ HEIGHT = CONFIG.get("HEIGHT")
 RESIZABLE = CONFIG.get("WINDOW_RESIZABLE")
 MAXIMIZABLE = CONFIG.get("WINDOW_MAXIMIZABLE")
 ALWAYS_ON_TOP = CONFIG.get("ALWAYS_ON_TOP")
+
 
 def main(page: ft.Page):
     global pages
@@ -53,145 +53,16 @@ def main(page: ft.Page):
         ]
     )
 
-    def route_change(*kwargs):
+    def RouteChange(*kwargs):
+
+        routes = Router.pages(page)
+
         page.views.clear()
         page.views.append(
-            pages[page.route]
+            routes[page.route]
         )
 
-    """
-     __   __  _______  __   __  _______ 
-    |  | |  ||       ||  |_|  ||       |
-    |  |_|  ||   _   ||       ||    ___|
-    |       ||  | |  ||       ||   |___ 
-    |       ||  |_|  ||       ||    ___|
-    |   _   ||       || ||_|| ||   |___ 
-    |__| |__||_______||_|   |_||_______|
-    """
-
-    Home = ft.Row(
-        controls=[
-            ft.FilledButton(text='Download audios', height=50,
-                            on_click=lambda _: page.go('/download_audios')),
-            ft.FilledButton(text='Download videos', height=50,
-                            on_click=lambda _: page.go('/download_videos')),
-        ],
-        height=page.window_height - 100,
-        width=page.window_width,
-        alignment=ft.MainAxisAlignment.CENTER
-    )
-
-    """
-     ______   _______  _     _  __    _  ___      _______  _______  ______  
-    |      | |       || | _ | ||  |  | ||   |    |       ||   _   ||      | 
-    |  _    ||   _   || || || ||   |_| ||   |    |   _   ||  |_|  ||  _    |
-    | | |   ||  | |  ||       ||       ||   |    |  | |  ||       || | |   |
-    | |_|   ||  |_|  ||       ||  _    ||   |___ |  |_|  ||       || |_|   |
-    |       ||       ||   _   || | |   ||       ||       ||   _   ||       |
-    |______| |_______||__| |__||_|  |__||_______||_______||__| |__||______|                                                                                                                                                                                       
-    """
-
-    qualities = ft.Row(alignment=ft.MainAxisAlignment.CENTER,
-                       spacing=40, scroll=ft.ScrollMode.AUTO)
-    NameTextField = ft.TextField(label='Name')
-    UrlTextField = ft.TextField(label='Url')
-
-    def get_resolutions(e):
-        global video
-        url = UrlTextField.value
-        qualities.clean()
-
-        def close_banner(e):
-            page.banner.open = False
-            page.update()
-
-        try:
-            resolutions, video = get_audio_resolution(url)
-
-            for resolution in resolutions:
-                qualities.controls.append(ft.Checkbox(label=resolution))
-
-            qualities.update()
-        except Exception as e:
-            page.banner = ft.Banner(
-                leading=ft.Icon(ft.icons.WARNING_AMBER_ROUNDED,
-                                color=ft.colors.AMBER, size=40),
-                content=ft.Text(
-                    "It seems that you have used an incorrect or invalid url, please check it and try again."
-                ),
-                actions=[
-                    ft.TextButton("Retry", on_click=close_banner)
-                ]
-            )
-
-            page.banner.open = True
-            page.update()
-
-    def download_mono_audios(e):
-        dic = {}
-        for checkBox in qualities.controls:
-            dic[checkBox.label] = checkBox.value
-
-        for key, value in dic.items():
-            if value:
-                    # Se realiza el llamado al snack_bar para decirle al usuario donde se guarda su archivo
-                out_path = download_audio(
-                    video, key, video_title=NameTextField.value)
-                page.snack_bar = ft.SnackBar(ft.Text(out_path))
-                page.snack_bar.open = True
-                sleep(0.2)
-                page.update()
-
-    DownloadAudios = ft.Container(
-        content=ft.Column(
-            controls=[
-                ft.Text('MonoLoad', text_align=ft.alignment.center),
-                NameTextField,
-                UrlTextField,
-                qualities,
-                ft.Row(
-                    controls=[
-                        ft.FilledButton(text='Resolution',
-                                        height=50, on_click=get_resolutions),
-                        ft.FilledButton(text='Download', height=50,
-                                        on_click=download_mono_audios)
-                    ],
-                    alignment=ft.MainAxisAlignment.CENTER,
-                    spacing=40
-                )
-            ],
-            height=page.window_height - 300,
-            spacing=20,
-            alignment=ft.MainAxisAlignment.CENTER
-        ),
-        width=page.window_width,
-        alignment=ft.alignment.center,
-        margin=10,
-        padding=10
-    )
-
-    pages = {
-        '/': ft.View(
-            '/', [
-                page.appbar,
-                Home
-            ]
-        ),
-        '/download_audios': ft.View(
-            '/download_audios', [
-                page.appbar,
-                DownloadAudios
-            ]
-        ),
-        '/download_videos': ft.View(
-            '/download_videos', [
-                page.appbar,
-                ft.Text('Download videos')
-            ]
-        )
-    }
-
-    page.on_route_change = route_change
+    page.on_route_change = RouteChange
     page.go('/')
     page.update()
 
